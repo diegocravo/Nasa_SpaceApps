c2bc81ad87c764b4caaf02447de0f5b5af388874
Diego <cravo.diego@gmail.com>
2021-10-03 03:20:15 -0300

    material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 import Photo from "./components/Photo";
+import TemporaryDrawer from "./components/TemporaryDrawer"
 
 function App() {
   return (
     <div>
-      <Header />
-      <Photo photo="https://apod.nasa.gov/apod/image/2110/M8-Pipe_APOD_GabrielSantos_LG.jpg" />
+      <TemporaryDrawer />
+      <Home />
+      {/* <Photo photo="https://apod.nasa.gov/apod/image/2110/M8-Pipe_APOD_GabrielSantos_LG.jpg" /> */}
     </div>
   );                                            
 }
